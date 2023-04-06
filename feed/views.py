152d92f5b1180e062c03ae70ca8dd763157fd847
nicob107 <124616658+nicob107@users.noreply.github.com>
2023-04-06 00:33:39 -0400

    Update views.py

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_objects_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
