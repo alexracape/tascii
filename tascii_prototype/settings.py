d67fc4fea902bf8b1328106577cbacaf7cb9c857
Alex Racapé <alex.racape@gmail.com>
2023-04-27 20:27:27 -0400

    Heroku stuff

--- tascii_prototype/settings.py
+++ tascii_prototype/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^!bd!d@@@gceu!$ib4k$29b=s@nncppje1w+wqide^83@%@8l'
+# SECRET_KEY = 'django-insecure-l^!bd!d@@@gceu!$ib4k$29b=s@nncppje1w+wqide^83@%@8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
